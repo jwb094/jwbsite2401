c327375cda10e91066c01cdcbcec87f644b2d3f6
Josh Bello <Jb230913@live.com>
2024-02-19 20:07:31 +0000

    update(clients) added styling for clients components / layout classes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let mix = require('laravel-mix');
 
 
-mix
-   js('public/js/gallery.js', 'public/js')
+mix.js('public/js/gallery_lightbox.js', 'public/js')
    .sass('public/css/main.scss', 'public/css');
